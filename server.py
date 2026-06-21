31b3af3aab8c396efd6f867b3697f49a9626038c
Naveeb <naveebpk132@gmail.com>
2026-06-21 14:53:27 +0530

    Add about page

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 
 app=Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 @app.route('/')
 def home():
@@ -9,6 +10,10 @@ def home():
 @app.route('/notes')
 def notes():
     return render_template('notes.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
