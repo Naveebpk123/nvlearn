426b4c9e1078c8839c613b9ae6c7ff69604c9d6c
Naveeb <naveebpk132@gmail.com>
2026-06-28 12:54:39 +0530

    Add close button to flash messages

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,14 @@
 const logo = document.getElementById('logo');
 
+const flashContainer = document.getElementById('flashContainer');
+const flashCloseBtn = document.getElementById('flashCloseBtn');
+
 function toggleSidebar() {
   document.getElementById("sidebar").classList.toggle("close");
   document.getElementById('contentWrapper').classList.toggle("sidebar-open");
 }
 logo.addEventListener('click',toggleSidebar);
+
+flashCloseBtn.addEventListener('click',()=>{
+  flashContainer.style.display = 'none';
+});
