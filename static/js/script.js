acd84224ddead8d4aac1f9f3a80836323d7634dd
Naveeb <naveebpk132@gmail.com>
2026-06-29 18:06:53 +0530

    Add confirm modal for logout and improve modal styling

--- static/js/script.js
+++ static/js/script.js
@@ -3,12 +3,36 @@ const logo = document.getElementById('logo');
 const flashContainer = document.getElementById('flashContainer');
 const flashCloseBtn = document.getElementById('flashCloseBtn');
 
+const modalBackground = document.getElementById('modalBackground'); //This is also container for the modal
+const modalText = document.getElementById('modalText');
+const modalCancelBtn = document.getElementById('modalCancelBtn');
+const modalConfirmBtn = document.getElementById('modalConfirmBtn');
+
+const logoutBtn = document.getElementById('sidebarLogout');
+
 function toggleSidebar() {
   document.getElementById("sidebar").classList.toggle("close");
   document.getElementById('contentWrapper').classList.toggle("sidebar-open");
 }
 logo.addEventListener('click',toggleSidebar);
 
+logoutBtn.addEventListener('click',(e)=>{
+  e.preventDefault();
+  modalBackground.style.display = 'flex';
+  modalText.innerText = 'Are you sure you want to logout?';
+  modalConfirmBtn.setAttribute('href','/logout');
+});
+
+modalConfirmBtn.addEventListener('click',()=>{
+  modalBackground.style.display = 'none';
+});
+modalCancelBtn.addEventListener('click',()=>{
+  modalBackground.style.display = 'none';
+  modalConfirmBtn.setAttribute('href','');
+});
+
 flashCloseBtn.addEventListener('click',()=>{
   flashContainer.style.display = 'none';
 });
+
+
