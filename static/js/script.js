e1e0372eb17a875cf3c29e66f2300d3946164fc8
Naveeb <naveebpk132@gmail.com>
2026-06-29 18:25:40 +0530

    Add confirm modal to delete note button

--- static/js/script.js
+++ static/js/script.js
@@ -1,38 +1,51 @@
-const logo = document.getElementById('logo');
+  const logo = document.getElementById('logo');
 
-const flashContainer = document.getElementById('flashContainer');
-const flashCloseBtn = document.getElementById('flashCloseBtn');
+  const flashContainer = document.getElementById('flashContainer');
+  const flashCloseBtn = document.getElementById('flashCloseBtn');
 
-const modalBackground = document.getElementById('modalBackground'); //This is also container for the modal
-const modalText = document.getElementById('modalText');
-const modalCancelBtn = document.getElementById('modalCancelBtn');
-const modalConfirmBtn = document.getElementById('modalConfirmBtn');
+  const modalBackground = document.getElementById('modalBackground'); //This is also container for the modal
+  const modalText = document.getElementById('modalText');
+  const modalCancelBtn = document.getElementById('modalCancelBtn');
+  const modalConfirmBtn = document.getElementById('modalConfirmBtn');
 
-const logoutBtn = document.getElementById('sidebarLogout');
+  const logoutBtn = document.getElementById('sidebarLogout');
 
-function toggleSidebar() {
-  document.getElementById("sidebar").classList.toggle("close");
-  document.getElementById('contentWrapper').classList.toggle("sidebar-open");
-}
-logo.addEventListener('click',toggleSidebar);
+  const deleteNoteBtn = document.getElementById('deleteNoteBtn');
 
-logoutBtn.addEventListener('click',(e)=>{
-  e.preventDefault();
-  modalBackground.style.display = 'flex';
-  modalText.innerText = 'Are you sure you want to logout?';
-  modalConfirmBtn.setAttribute('href','/logout');
-});
+  function toggleSidebar() {
+    document.getElementById("sidebar").classList.toggle("close");
+    document.getElementById('contentWrapper').classList.toggle("sidebar-open");
+  }
 
-modalConfirmBtn.addEventListener('click',()=>{
-  modalBackground.style.display = 'none';
-});
-modalCancelBtn.addEventListener('click',()=>{
-  modalBackground.style.display = 'none';
-  modalConfirmBtn.setAttribute('href','');
-});
+  function openModal(text,confirmBtnLink){
+    modalBackground.style.display = 'flex';
+    modalText.innerText = text;
+    modalConfirmBtn.setAttribute('href',confirmBtnLink);
+  }
 
-flashCloseBtn.addEventListener('click',()=>{
-  flashContainer.style.display = 'none';
-});
+  logo?.addEventListener('click',toggleSidebar);
+
+  logoutBtn?.addEventListener('click',(e)=>{
+    e.preventDefault();
+    openModal('Are you sure you want to logout?','/logout');
+
+  });
+
+  deleteNoteBtn?.addEventListener('click',(e)=>{
+    e.preventDefault();
+    openModal('Are you sure you want to permanently delete this note?',deleteNoteBtn.dataset.link);
+  })
+
+  modalConfirmBtn?.addEventListener('click',()=>{
+    modalBackground.style.display = 'none';
+  });
+  modalCancelBtn?.addEventListener('click',()=>{
+    modalBackground.style.display = 'none';
+    modalConfirmBtn.setAttribute('href','');
+  });
+
+  flashCloseBtn?.addEventListener('click',()=>{
+    flashContainer.style.display = 'none';
+  });
 
 
